e248af6bb19f3b9786f50bb861d67328c002324f
Martin Barend Hanekom <martin@martin-dell.martin.com>
2022-07-12 22:00:11 +0200

    firebase

---
+++ realtime-todos/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { signInWithPopup } from "firebase/auth";
+	import Profile from "./Profile.svelte";
+	//import Todos from "./Todos.svelte";
+
+	import { auth, provider } from "./firebase";
+	import { authState } from "rxfire/auth";
+
+	let user;
+
+	const unsubscribe = authState(auth).subscribe(u => user = u);
+
+	function login() {
+		signInWithPopup(auth, provider);
+	}
+</script>
+
+<section>
+	{#if user}
+		<Profile {...user} />
+		<button on:click={() => auth.signOut()}>Logout</button>
+		<hr>
+		<!--<Todos uid={user.uid} />-->
+	{:else}
+		<button on:click={login}>Signin with Google</button>
+	{/if}
+</section>
---
+++ realtime-todos/src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5lx2juoTidAuYhQhk9_C4DNih8thrCsY",
+  authDomain: "sveltetodo-4d505.firebaseapp.com",
+  projectId: "sveltetodo-4d505",
+  storageBucket: "sveltetodo-4d505.appspot.com",
+  messagingSenderId: "430707810439",
+  appId: "1:430707810439:web:070f61ef5f38b06be55822",
+  measurementId: "G-HZ7800XR33"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
