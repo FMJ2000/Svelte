262d2f62b8a7795d02bfe96eca8996304b329a79
Martin Barend Hanekom <martin@martin-dell.martin.com>
2022-07-12 21:09:29 +0200

    init_todos

---
+++ realtime-todos/src/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5lx2juoTidAuYhQhk9_C4DNih8thrCsY",
+  authDomain: "sveltetodo-4d505.firebaseapp.com",
+  projectId: "sveltetodo-4d505",
+  storageBucket: "sveltetodo-4d505.appspot.com",
+  messagingSenderId: "430707810439",
+  appId: "1:430707810439:web:070f61ef5f38b06be55822",
+  measurementId: "G-HZ7800XR33"
+};
+  
